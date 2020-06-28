f149c683f270ea4de328a2170d8a37108e5c48e9
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-06-28 17:31:36 -0400

    api-added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState , useEffect } from 'react';
+
+// data.results.title
 
 
-const apiUrl "https://gateway.marvel.com:443/v1/public/characters?apikey=7b8c4a6660c0ed6e66f01101cb400d55"
+const App = () => {
+  
+  //const apiKey = "7b8c4a6660c0ed6e66f01101cb400d55"
 
 
-function App() {
-  return (
-    <div className="App">
+  useEffect(() => {getHeroes()}, []);
+
+  const getHeroes = async () => {
+    const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?name=hulk&apikey=1ccf7656fa1a7c5e72470bb94658a746e15edd8d`)
+    const data = await response.json();
+    console.log(data)
    
+
+
+  }
+
+  return (
+    <div className="APP">
+      <form className='search-form'>
+        <input className='search-bar' type='text' />
+        <button className="search-button" type='submit'>
+          Search
+        </button>
+      </form>
     </div>
   );
-}
-
-export default App;
+ 
+};
+export default App
